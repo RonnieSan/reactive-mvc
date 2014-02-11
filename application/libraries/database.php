97494b61a75ea7601bfd425404fdb3be9f09bdfc
RonnieSan <ronniesan@ronniesan.com>
2014-02-11 09:27:18 -0800

    Updated result class

--- application/libraries/database.php
+++ application/libraries/database.php
@@ -325,16 +325,31 @@ Class Result
 	// Types can be ASSOC|BOTH|CLASS|OBJ|LAZY|NUM|FUNC
 	public function fetch_all($type = 'ASSOC', $obj = NULL) {
 
+		// Set the type
 		$type = constant("PDO::FETCH_{$type}");
-		return $this->_result->fetchAll($type);
 
+		if ($type == 8) {
+			if ($obj === NULL) {
+				$obj = 'stdClass';
+			}
+
+			return $this->_result->fetchAll($type, $obj);
+		} else {
+			unset($obj);
+		}
+		
+		return $this->_result->fetchAll($type);
 	}
 
 	// Fetch the next row of the results
 	public function fetch($type = 'ASSOC', $obj = NULL) {
 
+		if ($type == 'CLASS' && $obj !== NULL) {
+			$this->_result->setFetchMode(8, $obj);
+		}
+
 		$type = constant("PDO::FETCH_{$type}");
-		return $this->_result->fetch($type, $obj);
+		return $this->_result->fetch($type);
 
 	}
 
